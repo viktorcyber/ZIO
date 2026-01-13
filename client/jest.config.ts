1a28ac1361b9ed6a1397366a7d672c23578ec304
viktorcyber <zin.it.dev@gmail.com>
2026-01-13 20:45:38 +0700

    🐛 CI/CD

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -3,13 +3,13 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
-import nextJest from 'next/jest.js'
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 const config: Config = {
   // Automatically clear mock calls, instances, contexts and results before every test
@@ -19,20 +19,18 @@ const config: Config = {
   collectCoverage: true,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/__tests__/**/?(*.)+(spec|test).?([mc])[jt]s?(x)",
-  ],
+  testMatch: ['**/__tests__/**/?(*.)+(spec|test).?([mc])[jt]s?(x)'],
 
-  setupFilesAfterEnv: ['./src/__tests__/jest.setup.ts']
+  setupFilesAfterEnv: ['./src/__tests__/jest.setup.ts'],
 };
 
 export default createJestConfig(config);
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
-  return <div>
-    <h1>Hello</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
